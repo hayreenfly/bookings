fff9f691c55efaa7b90dbfb405b7c79b46d9dd8e
Irene Aguas <iaguas@quantifycrypto.net>
2021-12-31 16:12:25 -0800

    Added images and setup static file server

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 )
 
-func routes(app *config.AppConfig)http.Handler{
+func routes(app *config.AppConfig) http.Handler {
 	//mux := pat.New()
 	//
 	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
@@ -24,7 +24,9 @@ func routes(app *config.AppConfig)http.Handler{
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
-
